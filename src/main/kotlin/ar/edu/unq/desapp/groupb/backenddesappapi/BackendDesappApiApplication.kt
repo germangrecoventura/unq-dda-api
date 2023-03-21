473fb33253a6f5032fcca39c7d3ff4070f3b8857
germangrecoventura <germangv1998@gmail.com>
2023-03-21 19:50:32 -0300

    Initialize project

---
+++ src/main/kotlin/ar/edu/unq/desapp/groupb/backenddesappapi/BackendDesappApiApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.desapp.groupb.backenddesappapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendDesappApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendDesappApiApplication>(*args)
+}
