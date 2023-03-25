2602666854c1a31328e46b43f549a7f50f3d5f4f
germangrecoventura <germangv1998@gmail.com>
2023-03-25 13:30:59 -0300

    Enable request to localhost

---
+++ src/main/kotlin/ar/edu/unq/desapp/groupb/backenddesappapi/webservice/SecurityConfiguration.kt
@@ -0,0 +1,20 @@
+package ar.edu.unq.desapp.groupb.backenddesappapi.webservice
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+class SecurityConfiguration {
+    @Bean
+    @Throws(Exception::class)
+    fun configure(http: HttpSecurity): SecurityFilterChain? {
+        http.cors().and().csrf().disable()
+            .authorizeRequests().requestMatchers("/**").permitAll()
+        http.headers().frameOptions().disable()
+        return http.build()
+    }
+
+}
