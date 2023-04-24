4acf13cc1c1ada0e6b2df595e4e80f058d8e785e
germangrecoventura <germangv1998@gmail.com>
2023-04-24 13:02:39 -0300

    Add users a initializer

--- src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/Initializer.kt
+++ src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/Initializer.kt
@@ -1,6 +1,8 @@
 package ar.edu.unq.desapp.groupb.cryptop2p
 
 import ar.edu.unq.desapp.groupb.cryptop2p.service.AssetService
+import ar.edu.unq.desapp.groupb.cryptop2p.service.UserService
+import ar.edu.unq.desapp.groupb.cryptop2p.webservice.builder.UserCreateRequestDTOBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -9,12 +11,16 @@ class Initializer {
     @Autowired
     lateinit var assetService: AssetService
 
+    @Autowired
+    lateinit var userService: UserService
+
     fun cleanDataBase() {
         assetService.clear()
     }
 
     fun loadData() {
         loadAssets()
+        loadUsers()
     }
 
 
@@ -39,5 +45,38 @@ class Initializer {
         assets.forEach { asset ->
             assetService.save(asset)
         }
+    }
+
+
+    private fun loadUsers() {
+        fun anyUser(): UserCreateRequestDTOBuilder {
+            return UserCreateRequestDTOBuilder()
+                .withFirstName("Homero")
+                .withLastName("Simpson")
+                .withEmail("homero.simpson@sprinfield.com")
+                .withAddress("Evergreen 123")
+                .withPassword("Super!")
+                .withCVU("0011223344556677889900")
+                .withCryptoWallet("12345678")
+        }
+        userService.save(anyUser().build())
+        userService.save(
+            anyUser().withFirstName("German")
+                .withLastName("Greco")
+                .withEmail("german@gmail.com")
+                .withAddress("Andrade 40")
+                .withPassword("Super!")
+                .withCVU("0011223344556677289900")
+                .withCryptoWallet("12355678").build()
+        )
+        userService.save(
+            anyUser().withFirstName("Jose")
+                .withLastName("Fernandez")
+                .withEmail("fer@gmail.com")
+                .withAddress("Sarmiento 820")
+                .withPassword("Super!")
+                .withCVU("0011222344556677289900")
+                .withCryptoWallet("12355638").build()
+        )
     }
 }
