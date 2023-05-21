27614dce7b5512d03e539df3649e643f8ac4a65e
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2023-05-20 21:52:34 -0300

    Add query to report user transactions

--- src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/persistence/TransactionRepository.kt
+++ src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/persistence/TransactionRepository.kt
@@ -1,8 +1,21 @@
 package ar.edu.unq.desapp.groupb.cryptop2p.persistence
 
 import ar.edu.unq.desapp.groupb.cryptop2p.model.Transaction
+import ar.edu.unq.desapp.groupb.cryptop2p.model.TransactionStatus
+import ar.edu.unq.desapp.groupb.cryptop2p.model.User
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 @Repository
-interface TransactionRepository : CrudRepository<Transaction, Long>
+interface TransactionRepository : CrudRepository<Transaction, Long> {
+    @Query("SELECT t FROM Transaction t WHERE (t.seller = :user OR t.buyer = :user) AND t.created BETWEEN :created AND :created2 AND t.status = :status")
+    fun getAllBySellerOrBuyerAndCreatedBetweenAndStatus(
+        @Param("user") user: User,
+        @Param("created") created: LocalDateTime,
+        @Param("created2") created2: LocalDateTime,
+        @Param("status") status: TransactionStatus
+    ): List<Transaction>
+}
