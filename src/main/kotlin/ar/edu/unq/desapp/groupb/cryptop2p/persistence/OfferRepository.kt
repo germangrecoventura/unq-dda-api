f61f2b80ca231e22c0adb145277518835c9e0f69
Pablo <2845728+pablospizzamiglio@users.noreply.github.com>
2023-05-30 16:17:19 -0300

    Remove code smells (#13)
    * Remove unused imports
    
    * Reduce getActiveOffers method complexity
    
    * Improve data projection
    
    * Format SQL
    
    * Add custom OpenAPI spec path
    
    * Remove troublesome property

--- src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/persistence/OfferRepository.kt
+++ src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/persistence/OfferRepository.kt
@@ -1,9 +1,15 @@
 package ar.edu.unq.desapp.groupb.cryptop2p.persistence
 
 import ar.edu.unq.desapp.groupb.cryptop2p.model.Offer
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import java.util.Optional
 
 @Repository
-interface OfferRepository : CrudRepository<Offer, Long>
+interface OfferRepository : CrudRepository<Offer, Long> {
+    fun getByIsActive(isActive: Boolean): List<Offer>
+
+    @Query(value = "SELECT o FROM Offer o WHERE o.isActive = :isActive AND o.asset.name = :assetName")
+    fun getByIsActiveAndAsset(@Param("isActive") isActive: Boolean, @Param("assetName") assetName: String): List<Offer>
+}
--- src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/persistence/UserTransactionRatingRepository.kt
+++ src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/persistence/UserTransactionRatingRepository.kt
@@ -1,8 +1,21 @@
 package ar.edu.unq.desapp.groupb.cryptop2p.persistence
 
 import ar.edu.unq.desapp.groupb.cryptop2p.model.UserTransactionRating
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserTransactionRatingRepository : CrudRepository<UserTransactionRating, Long>
+interface UserTransactionRatingRepository : CrudRepository<UserTransactionRating, Long> {
+
+    @Query(value = "SELECT u.user.id AS userId, SUM(u.rating) AS rating, COUNT(u.user.id) AS totalOperations FROM UserTransactionRating AS u WHERE u.user.id IN :userIds GROUP BY u.user.id")
+    fun getByUserIn(@Param("userIds") userIds: Set<Long>): List<UserRating>
+
+}
+
+interface UserRating {
+    val userId: Long
+    val rating: Double
+    val totalOperations: Int
+}
