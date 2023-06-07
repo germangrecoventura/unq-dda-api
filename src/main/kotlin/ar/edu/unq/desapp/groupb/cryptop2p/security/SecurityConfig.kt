3213776af76619a952c01bbf3382eb30800f67d8
germangrecoventura <germangv1998@gmail.com>
2023-06-07 19:11:50 -0300

    Fix ui database h2

--- src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/security/SecurityConfig.kt
+++ src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/security/SecurityConfig.kt
@@ -6,13 +6,16 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+
 
 @Configuration
 @EnableWebSecurity
@@ -48,11 +51,18 @@ class SecurityConfig {
             .requestMatchers("/docs/**").permitAll()
             .requestMatchers("/v3/api-docs/**").permitAll()
             .requestMatchers("/swagger-ui/**").permitAll()
-            .requestMatchers("/h2-console/**").permitAll()
             .anyRequest().authenticated().and().httpBasic()
-
+        http.headers().frameOptions().disable()
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
+    }
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer? {
+        return WebSecurityCustomizer { web: WebSecurity ->
+            web.ignoring()
+                .requestMatchers(AntPathRequestMatcher("/h2-console/**"))
+        }
     }
 }
