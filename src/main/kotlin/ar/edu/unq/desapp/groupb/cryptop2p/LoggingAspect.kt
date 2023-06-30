469df8bfd43b9585f2047235489a4fd8d4ee22a1
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2023-06-30 20:41:54 -0300

    Add service audit aspect

--- src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/LoggingAspect.kt
+++ src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/LoggingAspect.kt
@@ -7,34 +7,49 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.reflect.MethodSignature
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.Authentication
-import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
-import java.time.LocalDate
+
 
 @Aspect
 @Component
 class LoggingAspect {
     private val logger: Logger = LogManager.getLogger(LoggingAspect::class.java)
 
-    //AOP expression for which methods shall be intercepted
+    // AOP expression for which methods shall be intercepted
     @Around("@within(org.springframework.web.bind.annotation.RestController)")
     @Throws(Throwable::class)
     fun profileAllMethods(proceedingJoinPoint: ProceedingJoinPoint): Any? {
         val stopWatch = StopWatch()
         stopWatch.start()
-        val methodSignature: MethodSignature = proceedingJoinPoint.signature as MethodSignature
-        //TODO: Funcion de busqueda email val user = getEmail(proceedingJoinPoint)
-        //Get intercepted method details
-        val className: String = methodSignature.declaringType.simpleName
-        val methodName: String = methodSignature.name
-        //Measure method execution time
+
+        // Get authentication from security context
+        val authentication = SecurityContextHolder.getContext().authentication
+        val userName = authentication.name
+
+        // Get intercepted method details
+        val methodSignature = proceedingJoinPoint.signature as MethodSignature
+        val className = methodSignature.declaringType.simpleName
+        val methodName = methodSignature.name
+        val params = methodSignature.parameterNames.toList()
+        val args = proceedingJoinPoint.args
+
+        // Map param names with args values
+        assert(args.size == params.size)
+        val paramArgs = mutableListOf<String>()
+        args.indices.forEach { argIndex ->
+            paramArgs.add("${params[argIndex]} = ${args[argIndex]}")
+        }
+
+        // Get intercepted method result
         val result = proceedingJoinPoint.proceed()
-        val params = (proceedingJoinPoint.signature as MethodSignature).parameterNames.toList()
+
+        // Measure intercepted method execution time
         stopWatch.stop()
-        //Log method execution time
-        logger.info("Request made: " + LocalDate.now() + " - $className" + "." + methodName + " - params: $params " + " :: " + stopWatch.time + " ms")
+
+        // Log intercepted method execution time
+        logger.info("User: $userName :: $className.$methodName(${paramArgs.joinToString(", ")}) :: Execution time: ${stopWatch.time} ms")
+
         return result
     }
 }
