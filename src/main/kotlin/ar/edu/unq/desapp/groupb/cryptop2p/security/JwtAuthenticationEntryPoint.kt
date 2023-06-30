e77a3374c2b2c3def9ddfd3859814438f6c4e539
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2023-06-30 20:41:21 -0300

    Format files

--- src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/security/JwtAuthenticationEntryPoint.kt
+++ src/main/kotlin/ar/edu/unq/desapp/groupb/cryptop2p/security/JwtAuthenticationEntryPoint.kt
@@ -6,8 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import java.io.IOException
@@ -29,8 +27,6 @@ class JwtAuthenticationEntryPoint : AuthenticationEntryPoint {
                 )
             )
         )
-        var logger: Logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint::class.java)
-        logger.error(json)
         response.writer.write(json)
     }
 }
